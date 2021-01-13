dc6ddd50c33e91a44fdd70ae5fd6b8e8f5f954c4
kastrahl <56175823+kastrahl@users.noreply.github.com>
2021-01-13 10:03:33 +0530

    using 'open' keyword to inherit classes

---
+++ UNIT2/partA/heirarchy4.kt
@@ -0,0 +1,45 @@
+fun main() {
+    val squareCabin = SquareCabin(5)
+    val roundHut = RoundHut(3)
+    val roundTower = RoundTower(4)
+
+    with(squareCabin) {
+    println("\nSquare Cabin\n============")
+    println("Capacity: ${capacity}")
+    println("Material: ${buildingMaterial}")
+    println("Has room? ${hasRoom()}")
+    }
+    with(roundHut) {
+    println("\nRound Hut\n=========")
+    println("Material: ${buildingMaterial}")
+    println("Capacity: ${capacity}")
+    println("Has room? ${hasRoom()}")
+    }
+    with(roundTower) {
+    println("\nRound Tower\n==========")
+    println("Material: ${buildingMaterial}")
+    println("Capacity: ${capacity}")
+    println("Has room? ${hasRoom()}")
+}
+}
+abstract class Dwelling(private var residents: Int){ //not defined here(abstract)
+    abstract val buildingMaterial: String		//not defined here(abstract)
+    abstract val capacity: Int					//not defined here(abstract)
+    
+    fun hasRoom(): Boolean {			//hasRoom() function was inherited for free
+    return residents < capacity
+    }
+}
+/*constructors*/
+class SquareCabin(residents: Int)/*class parameter */: Dwelling(residents){ //passing already declared RESIDENTS 
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
+}
+open class RoundHut(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Straw"
+    override val capacity = 4
+}
+class RoundTower(residents: Int) : RoundHut(residents) {	//class inherited from an OPEN CLASS ROUNDHUT
+    override val buildingMaterial = "Stone"
+    override val capacity = 4
+}
